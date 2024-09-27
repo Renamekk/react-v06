8036bb689a4202234210bac05df8f766d15f32b6
Renamekk <black80512@gmail.com>
2024-09-27 18:06:37 +0200

    goit-react-hw-06

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,22 @@
 import Contact from "../Contact/Contact.jsx";
 import css from "./ContactList.module.css";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice.js";
+import { selectFilters } from "../../redux/filtersSlice.js";
 
-export default function ContactList({ phoneBook, onDelete }) {
+export default function ContactList() {
+  const phoneBook = useSelector(selectContacts);
+  const filteredBook = useSelector(selectFilters);
+
+  const visibleContacts = phoneBook.filter(phone =>
+    phone.name.toLowerCase().includes(filteredBook.toLowerCase())
+  );
+
   return (
     <ul className={css.contactList}>
-      {phoneBook.map(data => (
+      {visibleContacts.map(data => (
         <li key={data.id} className={css.contact}>
-          <Contact data={data} onDelete={onDelete} />
+          <Contact data={data} />
         </li>
       ))}
     </ul>
